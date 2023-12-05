f48d11adee49b32f448040e9d07e9c97a4481f5b
Vladimir <110772568+Vova4o@users.noreply.github.com>
2023-12-06 02:36:16 +0300

    first upload

---
+++ main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"html/template"
+	"net/http"
+)
+
+type Article struct {
+	Id           uint16
+	Title, Anons string
+}
+
+var note = []Article{}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("templates/index.html", "templates/header.html", "templates/footer.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/golang")
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	res, err := db.Query("SELECT * FROM `notes`")
+	if err != nil {
+		panic(err)
+	}
+
+	for res.Next() {
+		var post Article
+		err = res.Scan(&post.Id, &post.Title, &post.Anons)
+		if err != nil {
+			panic(err)
+		}
+		//fmt.Println(fmt.Sprintf("Post: %s with id: %d", post.Title, post.Id))
+		note = append(note, post)
+	}
+
+	t.ExecuteTemplate(w, "index", note)
+}
+
+func create(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("templates/create.html", "templates/header.html", "templates/footer.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+
+	t.ExecuteTemplate(w, "create", nil)
+}
+
+func save_article(w http.ResponseWriter, r *http.Request) {
+	title := r.FormValue("title")
+	anons := r.FormValue("anons")
+
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/golang")
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	insert, err := db.Query(fmt.Sprintf("INSERT INTO `notes` (`title`, `anons`) VALUES ('%s','%s')", title, anons))
+	if err != nil {
+		panic(err)
+	}
+	defer insert.Close()
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+func handleFunc() {
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
+	http.HandleFunc("/", index)
+	http.HandleFunc("/create", create)
+	http.HandleFunc("/save_article", save_article)
+	http.ListenAndServe(":8080", nil)
+}
+
+func main() {
+	handleFunc()
+}
